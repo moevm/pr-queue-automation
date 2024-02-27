459d5457c35575b5de0591f35097bb67bad47890
KozlovGeorgii <abonent-21@yandex.ru>
2024-02-27 21:04:56 +0300

    Добавил возможность изменять гугл документ

--- data_handler.py
+++ data_handler.py
@@ -1,29 +1,31 @@
 from scrtip_parsing_pr import ParseDataGit
 from jinja2 import Environment, FileSystemLoader
-from google_sheet_data_handeler import get_name_discord_acc_dict
+from google_sheet_data_handeler import get_name_discord_acc_dict, get_name_status_work_dict, change_data_in_goodle_sheet
 
 
 
 class DataHandlerGit(ParseDataGit):
-    def __init__(self,token1, name_repo, web_path_to_discord_table=None):
+    def __init__(self,token1, name_repo, sheet_google_token):
         super().__init__(token=token1, repo_name=name_repo)
-        self.path_to_result_temp_message = None # путь до шаблона после собработки скриптом
-        self.path_to_temp_message = None # путь до jinja скрипта с шаблоном
-        self.path_table_discord_names = web_path_to_discord_table # ссылка на гугл таблицу
+        self.sheet_google_token = sheet_google_token
+        # 1WEX-4FBdcUHsJpf7ybKZP52R3X4SfEqdJ8xM-1Tbxt8
+        # Workdata
+        # A2:B
 
-
-    def generate_temp_messasge(self, type_work):
-        path_temp_message='/'.join(self.path_to_temp_message.split('/')[:-1])
-        name_of_file = self.path_to_temp_message.split('/')[-1]
-        path_res_temp_message=self.path_to_result_temp_message
+    def generate_temp_messasge(self, type_work: str, script_path: str, result_path: str, name_google_list: str, range_data: str):
+        path_to_result_temp_message = script_path # путь до шаблона после собработки скриптом
+        path_to_temp_message = result_path
+        path_temp_message='/'.join(path_to_temp_message.split('/')[:-1])
+        name_of_file = path_to_temp_message.split('/')[-1]
+        path_res_temp_message = path_to_result_temp_message
         if path_res_temp_message is None or path_res_temp_message is None: # проверка на наличие пути к шаблонам
             print("Set path to result and script message")
             raise Exception
         name_status_list_of_dict = []
         students_status = self.get_status_to_proctering() # cловарь с именами и статусами допуска
-        discord_login = get_name_discord_acc_dict(sheet_key='1WEX-4FBdcUHsJpf7ybKZP52R3X4SfEqdJ8xM-1Tbxt8',
-                          name_list='Workdata',
-                          range_data='A2:B')
+        discord_login = get_name_discord_acc_dict(sheet_key=self.sheet_google_token, # ключ к оределенному гугл ссылке докмента
+                          name_list=name_google_list, # название листа в документе
+                          range_data=range_data) # промежутки считывания информации
 
 
         for student in students_status:
@@ -49,33 +51,26 @@ class DataHandlerGit(ParseDataGit):
         with open(results_filename, mode="w", encoding="utf-8") as results: 
             results.write(template.render(context))
             print(f"Data has been written to {results_filename}")
+
         
-    def set_path_to_temp_message(self, path):
-        self.path_to_temp_message = path
-	
-    def get_path_to_temp_message(self):
-        return self.path_to_temp_message
-    
-    def set_path_to_res_temp_message(self, path):
-        self.path_to_result_temp_message = path
-    
-    def get_path_to_res_temp_message(self):
-        return self.path_to_result_temp_message
-    
-    def set_path_to_table_discord_names(self, path):
-        self.path_table_discord_names = path
-	
-    def get_path_to_table_discord_names(self):
-        return self.path_table_discord_names
-    
-    def get_data_from_google_sheet():
-        pass
+    def update_status_to_proctoring(self, name_list_sheet):
+        status_to_proctering_from_git = self.get_status_to_proctering()
+        status_to_procteting_from_gs = get_name_status_work_dict(sheet_key=self.sheet_google_token, name_list=name_list_sheet)
+
 
 if __name__ == '__main__':
-    a = DataHandlerGit(token1='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',name_repo='test_repo')
-    a.set_path_to_res_temp_message('templates/result_templates/result_template.txt')
-    a.set_path_to_temp_message('templates/script_templates/temp_for_lb.txt')
-    a.generate_temp_messasge(type_work='lb')
+    a = DataHandlerGit(token1='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',name_repo='test_repo', 
+                       sheet_google_token='1WEX-4FBdcUHsJpf7ybKZP52R3X4SfEqdJ8xM-1Tbxt8')
+    name_list = 'Workdata'
+    r_data = 'A2:B'
+    script_text = 'templates/script_templates/temp_for_lb.txt'
+    res_text = 'templates/result_templates/result_template.txt'
+    # a.generate_temp_messasge(type_work='lb', script_path=script_text, result_path=res_text,
+    #                          name_google_list=name_list, range_data=r_data)
+    # print(a.update_status_to_proctoring(name_list_sheet="Workdata"))
+    change_data_in_goodle_sheet(sheet_key='1WEX-4FBdcUHsJpf7ybKZP52R3X4SfEqdJ8xM-1Tbxt8', 
+                                name_list='Workdata', cell='D2', data_in_cell='hello 123', 
+                                gid=0, color={'green':1, 'blue':1, 'red':1})
         
 
         
--- google_sheet_data_handeler.py
+++ google_sheet_data_handeler.py
@@ -10,7 +10,7 @@ httpAuth = credentials.authorize(httplib2.Http()) # Авторизуемся в 
 service = build('sheets', 'v4', http = httpAuth) # Выбираем работу с таблицами и 4 версию API 
 
 
-def get_name_discord_acc_dict(sheet_key, name_list, range_data,):
+def get_name_discord_acc_dict(sheet_key, name_list, range_data='A2:B'):
     name_login_dicord = {}
     ranges = [f"{name_list}!{range_data}"]       
     results = service.spreadsheets().values().batchGet(spreadsheetId = sheet_key, 
@@ -21,3 +21,73 @@ def get_name_discord_acc_dict(sheet_key, name_list, range_data,):
     for i in sheet_values:
         name_login_dicord[i[0]] = i[1]
     return name_login_dicord
+
+def get_name_status_work_dict(sheet_key, name_list, range_data="A2:Z"):
+    status_work = {}
+    ranges = [f"{name_list}!{range_data}"]       
+    results = service.spreadsheets().values().batchGet(spreadsheetId = sheet_key, 
+                                        ranges = ranges, #  задаем промежуток считывания
+                                        valueRenderOption = 'FORMATTED_VALUE',  
+                                        dateTimeRenderOption = 'FORMATTED_STRING').execute() 
+    sheet_values = results['valueRanges'][0]['values']
+    for data_student in sheet_values:
+        status_work[data_student[0]] = {}
+        num_work = 1
+        for status in data_student[3::2]:
+            if status.lower() != 'допущен':
+                status_work[data_student[0]][f'lb_{num_work}'] = ''
+            else:
+                status_work[data_student[0]][f'lb_{num_work}'] = 'допущен'
+            num_work += 1
+        if data_student[-1].lower() != 'допущен':
+            status_work[data_student[0]]['cw'] = ''
+        else:
+            status_work[data_student[0]]['cw'] = 'допущен'
+    return status_work
+
+
+def change_data_in_goodle_sheet(sheet_key, name_list, cell, data_in_cell:str, gid, color:dict):
+    # Установка формата ячеек
+    letter, number = cell[0], cell[1]
+    column, row = ord(letter) % 65, int(number) - 1
+    service.spreadsheets().batchUpdate(
+        spreadsheetId = sheet_key,
+        body = 
+    {
+    "requests": 
+    [
+        {
+        "repeatCell": 
+        {
+            "cell": 
+            {
+            "userEnteredFormat": 
+                {
+                "backgroundColor": color
+                }
+            },
+            "range": 
+            {
+            "sheetId": gid,
+            "startRowIndex": row,
+            "endRowIndex": row + 1,
+            "startColumnIndex": column,
+            "endColumnIndex": column + 1,
+            },
+            "fields": "userEnteredFormat"
+        }
+        }
+    ]
+    }).execute()
+    service.spreadsheets().values().batchUpdate(spreadsheetId = sheet_key, body = {
+    "valueInputOption": "USER_ENTERED", # Данные воспринимаются, как вводимые пользователем (считается значение формул)
+    "data": [
+        {"range": f"{name_list}!{cell}",
+         "majorDimension": "ROWS",     # Сначала заполнять строки, затем столбцы
+         "values": [
+                    [f"{data_in_cell}"] # Заполняем вторую строку
+                   ]
+        
+        }
+    ]
+    }).execute()
